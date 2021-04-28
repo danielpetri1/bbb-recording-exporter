8e63b1f846239a249f59701f3cb35018310b615e
Daniel Petri Rocha <daniel.petri@tum.de>
2021-04-28 11:04:18 +0200

    ran Rubocop on code

--- presentation-render.rb
+++ presentation-render.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'nokogiri'
 require 'open-uri'
 require 'cgi'
@@ -6,33 +8,33 @@ require 'fileutils'
 # Reference recording: "https://balancer.bbb.rbg.tum.de/playback/presentation/2.3/f5c1fdc86039b1cd48cb686d38ec0eb6be27dfc7-1619030802001?meetingId=f5c1fdc86039b1cd48cb686d38ec0eb6be27dfc7-1619030802001"
 
 def download(file)
-    # Format: "https://hostname/presentation/meetingID/file"
-    # path = "https://balancer.bbb.rbg.tum.de/presentation/f5c1fdc86039b1cd48cb686d38ec0eb6be27dfc7-1619030802001/" # If not indicated through CLI
+  # Format: "https://hostname/presentation/meetingID/file"
+  # path = "https://balancer.bbb.rbg.tum.de/presentation/f5c1fdc86039b1cd48cb686d38ec0eb6be27dfc7-1619030802001/" # If not indicated through CLI
 
-    uri = URI.parse(ARGV[0])
-    meetingId = CGI.parse(uri.query)['meetingId'].first
-    path = URI::HTTP.build(:scheme => uri.scheme, :host => uri.host, :path => '/presentation/' + meetingId + '/' + file)
+  uri = URI.parse(ARGV[0])
+  meetingId = CGI.parse(uri.query)['meetingId'].first
+  path = URI::HTTP.build(scheme: uri.scheme, host: uri.host, path: "/presentation/#{meetingId}/#{file}")
 
-    puts "Downloading " + path.to_s
+  puts "Downloading #{path}"
 
-    open(file, 'wb') do |file|
-        file << open(path).read
-    end
+  open(file, 'wb') do |file|
+    file << open(path).read
+  end
 end
 
 # Download desired data, e.g. 'metadata.xml', 'panzooms.xml', 'cursor.xml', 'presentation_text.json', 'captions.json', 'slides_new.xml', 'video/webcams.mp4', 'deskshare/deskshare.mp4'
-for get in ['shapes.svg', 'video/webcams.mp4', 'deskshare/deskshare.mp4'] do
-    download(get)
+['shapes.svg', 'video/webcams.mp4', 'deskshare/deskshare.mp4'].each do |get|
+  download(get)
 end
 
 # Opens shapes.svg
-@doc = Nokogiri::XML(File.open("shapes.svg"))
+@doc = Nokogiri::XML(File.open('shapes.svg'))
 
 # Creates new file to hold the timestamps of the whiteboard
-File.open("whiteboard-timestamps", "w") {}
+File.open('whiteboard-timestamps', 'w') {}
 
 # Creates new file to hold the timestamps of the slides
-File.open("presentation-timestamps", "w") {}
+File.open('presentation-timestamps', 'w') {}
 
 # Gets each slide in the presentation
 slides = @doc.xpath('//xmlns:image', 'xmlns' => 'http://www.w3.org/2000/svg', 'xlink' => 'http://www.w3.org/1999/xlink')
@@ -40,14 +42,12 @@ slides = @doc.xpath('//xmlns:image', 'xmlns' => 'http://www.w3.org/2000/svg', 'x
 # Downloads each slide
 # On the server you don't have to do this since the slides are saved as individual PNG images in the presentation folder
 slides.each do |img|
-    path = File.dirname(img.attr('xlink:href'))
-    
-    # Creates folder structure if it's not yet present
-    unless File.directory?(path) 
-        FileUtils.mkdir_p(path)
-    end
-    
-    download(img.attr('xlink:href'))
+  path = File.dirname(img.attr('xlink:href'))
+
+  # Creates folder structure if it's not yet present
+  FileUtils.mkdir_p(path) unless File.directory?(path)
+
+  download(img.attr('xlink:href'))
 end
 
 # Gets each canvas drawn over the presentation
@@ -60,161 +60,167 @@ slideNumber = 0
 
 # For each slide, we write down the time it appears in the animation and create an SVG file displaying it
 slides.each do |slide|
-    # Get slide's background image
-    image = slide.attr('xlink:href')
+  # Get slide's background image
+  image = slide.attr('xlink:href')
 
-    # Get SVG width and height
-    width = slide.attr('width')
-    height = slide.attr('height')
+  # Get SVG width and height
+  width = slide.attr('width')
+  height = slide.attr('height')
 
-    # How long the presentation slide is displayed for
-    duration = (slide.attr('out').to_f - slide.attr('in').to_f).round(1)
+  # How long the presentation slide is displayed for
+  duration = (slide.attr('out').to_f - slide.attr('in').to_f).round(1)
 
-    # Background image
-    File.open('presentation-timestamps', 'a') do |file|
-        file.puts "file '" + image + "'"
-        file.puts "duration " + duration.to_s
+  # Background image
+  File.open('presentation-timestamps', 'a') do |file|
+    file.puts "file '#{image}'"
+    file.puts "duration #{duration}"
+  end
+
+  # Whiteboard: if the current slide has annotations drawn over it, we need to recreate the animation's frames
+  if hasCanvas.include? slide.attr('id')
+    # Finds correct frame elements given id
+    frames = @doc.xpath("//*[@image=\"#{slide.attr('id')}\"]/*")
+
+    # Get timing information
+    canvasStart = frames.xpath('./@timestamp')[0]
+    canvasEnd = frames.xpath('./@timestamp').pop
+
+    # Creates first slide, with no annotations
+    builder = Nokogiri::XML::Builder.new do |xml|
+      xml.svg(width: width, height: height, version: '1.1', 'xmlns' => 'http://www.w3.org/2000/svg',
+              'xmlns:xlink' => 'http://www.w3.org/1999/xlink') do
+      end
     end
 
-    # Whiteboard: if the current slide has annotations drawn over it, we need to recreate the animation's frames
-    if hasCanvas.include? slide.attr('id')
-        # Finds correct frame elements given id
-        frames = @doc.xpath("//*[@image=\"" + slide.attr('id').to_s + "\"]/*")
+    # Saves empty frame as SVG file
+    File.open("frames/frame#{frameNumber}.svg", 'w') do |file|
+      file.write(builder.to_xml)
+    end
 
-        # Get timing information
-        canvasStart = frames.xpath('./@timestamp')[0]
-        canvasEnd = frames.xpath('./@timestamp').pop
-        
-        # Creates first slide, with no annotations
-        builder = Nokogiri::XML::Builder.new do |xml|
-            xml.svg(width: width, height: height, version: "1.1", 'xmlns' => "http://www.w3.org/2000/svg", 'xmlns:xlink' => "http://www.w3.org/1999/xlink") {
-            }
-        end
+    duration = (canvasStart.to_s.to_f - slide.attr('in').to_f).round(1)
 
-        # Saves empty frame as SVG file
-        File.open("frames/frame" + frameNumber.to_s + ".svg", "w") do |file|
-            file.write(builder.to_xml)
-        end
+    # Prevents negative timestamps from making it into the file (must be fixed, has something to do with undo / going back in presentation)
+    if duration.negative?
+      duration = (slide.attr('out').to_f - slide.attr('in').to_f).round(1)
 
-        duration = (canvasStart.to_s.to_f - slide.attr('in').to_f).round(1)
+      File.open('whiteboard-timestamps', 'a') do |file|
+        file.puts "file frames/frame#{frameNumber}.svg"
+        file.puts "duration #{duration}"
+      end
 
-        # Prevents negative timestamps from making it into the file (must be fixed, has something to do with undo / going back in presentation)
-        if duration < 0 then
-            duration = (slide.attr('out').to_f - slide.attr('in').to_f).round(1)
+      next
+    end
 
-            File.open('whiteboard-timestamps', 'a') do |file|
-                file.puts "file frames/frame" + frameNumber.to_s + ".svg"
-                file.puts "duration " + duration.to_s
-            end
+    File.open('whiteboard-timestamps', 'a') do |file|
+      file.puts "file frames/frame#{frameNumber}.svg"
+      file.puts "duration #{duration}"
+    end
 
-            next
-        end
+    frameNumber += 1
 
-        File.open('whiteboard-timestamps', 'a') do |file|
-            file.puts "file frames/frame" + frameNumber.to_s + ".svg"
-            file.puts "duration " + duration.to_s
-        end
+    # Draw the frames
+    frameTimings = frames.xpath('./@timestamp').to_a.map(&:to_s).map(&:to_f).each_cons(2).map do |a, b|
+      (b - a).round(1)
+    end << (slide.attr('out').to_f - canvasEnd.to_s.to_f).round(1)
 
-        frameNumber += 1
+    frames.each do |frame|
+      # A frame is consists of the current drawn asset and the ones that came before it
+      frameSiblings = frame.xpath('./self::*|preceding-sibling::*')
 
-        # Draw the frames
-        frameTimings = frames.xpath('./@timestamp').to_a.map(&:to_s).map(&:to_f).each_cons(2).map { |a, b| (b-a).round(1) } << (slide.attr('out').to_f - canvasEnd.to_s.to_f).round(1)
-
-        frames.each do |frame|
-            # A frame is consists of the current drawn asset and the ones that came before it
-            frameSiblings = frame.xpath('./self::*|preceding-sibling::*')
-
-            # All frame nodes need to be set to visible too
-            frameSiblings.each do |node|
-                style = node.attr('style')
-                style.sub! 'hidden', 'visible'
-                node.set_attribute('style', style)
-            end
-
-            # Builds SVG frame
-            builder = Nokogiri::XML::Builder.new do |xml|
-                xml.svg(width: width, height: height, version: "1.1", 'xmlns' => "http://www.w3.org/2000/svg", 'xmlns:xlink' => "http://www.w3.org/1999/xlink") {
-                    # Adds whiteboard
-                    xml << frameSiblings.to_s
-                }
-            end
-
-            # Saves frame as SVG file
-            File.open("frames/frame" + frameNumber.to_s + ".svg", "w") do |file|
-                file.write(builder.to_xml)
-            end
-
-            # Duration of frame is the next element in the frameTimings queue
-            duration = frameTimings.shift
-
-            File.open('whiteboard-timestamps', 'a') do |file|
-                file.puts("file frames/frame" + frameNumber.to_s + ".svg")
-                file.puts "duration " + duration.to_s
-            end
-
-            frameNumber += 1
-        end
-        
-        # Export last frame as PNG
-        frameSiblings = frames.last.xpath('./self::*|preceding-sibling::*')
-
-        # The background image needs to be made visible
-        style = frames.last.attr('style')
+      # All frame nodes need to be set to visible too
+      frameSiblings.each do |node|
+        style = node.attr('style')
         style.sub! 'hidden', 'visible'
-        slide.set_attribute('style', style)
+        node.set_attribute('style', style)
+      end
 
-        # All frame nodes need to be set to visible too
-        frameSiblings.each do |node|
-            style = node.attr('style')
-            style.sub! 'hidden', 'visible'
-            node.set_attribute('style', style)
+      # Builds SVG frame
+      builder = Nokogiri::XML::Builder.new do |xml|
+        xml.svg(width: width, height: height, version: '1.1', 'xmlns' => 'http://www.w3.org/2000/svg',
+                'xmlns:xlink' => 'http://www.w3.org/1999/xlink') do
+          # Adds whiteboard
+          xml << frameSiblings.to_s
         end
+      end
 
-        builder = Nokogiri::XML::Builder.new do |xml|
-            xml.svg(width: width, height: height, version: "1.1", 'xmlns' => "http://www.w3.org/2000/svg", 'xmlns:xlink' => "http://www.w3.org/1999/xlink") {
-                # Adds background image
-                xml << slide.to_s
-                
-                # Adds whiteboard
-                xml << frameSiblings.to_s
-        }
-        end
+      # Saves frame as SVG file
+      File.open("frames/frame#{frameNumber}.svg", 'w') do |file|
+        file.write(builder.to_xml)
+      end
 
-        File.open("tmp.svg", "w") do |file|
-            file.write(builder.to_xml)
-        end
-        
-        #Exports last frame as PNG
-        export = "rsvg-convert --format=png --output=slides/slide" + slideNumber.to_s + ".png tmp.svg"
-        system(export)
+      # Duration of frame is the next element in the frameTimings queue
+      duration = frameTimings.shift
 
-    else
-        # Don't show any annotations - empty SVG frame
-        builder = Nokogiri::XML::Builder.new do |xml|
-            xml.svg(width: width, height: height, version: "1.1", 'xmlns' => "http://www.w3.org/2000/svg", 'xmlns:xlink' => "http://www.w3.org/1999/xlink") {
-            }
-        end
+      File.open('whiteboard-timestamps', 'a') do |file|
+        file.puts("file frames/frame#{frameNumber}.svg")
+        file.puts "duration #{duration}"
+      end
 
-        # Saves empty frame as SVG file
-        File.open("frames/frame" + frameNumber.to_s + ".svg", "w") do |file|
-            file.write(builder.to_xml)
-        end
-
-        # Adds transparent frame to the whiteboard file
-        File.open('whiteboard-timestamps', 'a') do |file|
-            file.puts "file frames/frame" + frameNumber.to_s + ".svg"
-            file.puts "duration " + duration.to_s
-        end
-
-        # Saves slide for later PDF export
-        open('slides/slide' + slideNumber.to_s + '.png', 'wb') do |file|
-            file << open(image).read
-        end
-        
-        frameNumber += 1
+      frameNumber += 1
     end
-    
-    slideNumber += 1
+
+    # Export last frame as PNG
+    frameSiblings = frames.last.xpath('./self::*|preceding-sibling::*')
+
+    # The background image needs to be made visible
+    style = frames.last.attr('style')
+    style.sub! 'hidden', 'visible'
+    slide.set_attribute('style', style)
+
+    # All frame nodes need to be set to visible too
+    frameSiblings.each do |node|
+      style = node.attr('style')
+      style.sub! 'hidden', 'visible'
+      node.set_attribute('style', style)
+    end
+
+    builder = Nokogiri::XML::Builder.new do |xml|
+      xml.svg(width: width, height: height, version: '1.1', 'xmlns' => 'http://www.w3.org/2000/svg',
+              'xmlns:xlink' => 'http://www.w3.org/1999/xlink') do
+        # Adds background image
+        xml << slide.to_s
+
+        # Adds whiteboard
+        xml << frameSiblings.to_s
+      end
+    end
+
+    File.open('tmp.svg', 'w') do |file|
+      file.write(builder.to_xml)
+    end
+
+    # Exports last frame as PNG
+    export = "rsvg-convert --format=png --output=slides/slide#{slideNumber}.png tmp.svg"
+    system(export)
+
+  else
+    # Don't show any annotations - empty SVG frame
+    builder = Nokogiri::XML::Builder.new do |xml|
+      xml.svg(width: width, height: height, version: '1.1', 'xmlns' => 'http://www.w3.org/2000/svg',
+              'xmlns:xlink' => 'http://www.w3.org/1999/xlink') do
+      end
+    end
+
+    # Saves empty frame as SVG file
+    File.open("frames/frame#{frameNumber}.svg", 'w') do |file|
+      file.write(builder.to_xml)
+    end
+
+    # Adds transparent frame to the whiteboard file
+    File.open('whiteboard-timestamps', 'a') do |file|
+      file.puts "file frames/frame#{frameNumber}.svg"
+      file.puts "duration #{duration}"
+    end
+
+    # Saves slide for later PDF export
+    open("slides/slide#{slideNumber}.png", 'wb') do |file|
+      file << open(image).read
+    end
+
+    frameNumber += 1
+  end
+
+  slideNumber += 1
 end
 
 # Recreates the presentation slides

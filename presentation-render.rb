4d07a7513b54d9a1805ebbdc8c6a165377426753
Daniel Petri Rocha <daniel.petri@tum.de>
2021-04-30 12:19:07 +0200

    Annotations for circles, rectangles etc. work

--- presentation-render.rb
+++ presentation-render.rb
@@ -118,11 +118,11 @@ slides.each do |slide|
 
     frameNumber += 1
 
-    # Draw the frames
-    frameTimings = frames.xpath('./@timestamp').to_a.map(&:to_s).map(&:to_f).each_cons(2).map do |a, b|
-      (b - a).round(1)
+    
+    frameTimings = frames.xpath('./@timestamp').to_a.map(&:to_s).map(&:to_f).each_cons(2).map do |a, b| (b - a).round(1)
     end << (slide.attr('out').to_f - canvasEnd.to_s.to_f).round(1)
-
+    
+    # Draw the frames
     frames.each do |frame|
       # A frame is consists of the current drawn asset and the ones that came before it
       frameSiblings = frame.xpath('./self::*|preceding-sibling::*')
@@ -224,13 +224,13 @@ slides.each do |slide|
 end
 
 # Recreates the presentation slides
-system("ffmpeg -f concat -i presentation-timestamps -c:v libvpx-vp9 -b:v 2M -pix_fmt yuva420p -metadata:s:v:0 alpha_mode=\"1\" -vsync vfr -auto-alt-ref 0 -y -filter_complex 'scale=w=1280:h=720:force_original_aspect_ratio=1,pad=1280:720:-1:-1:white' presentation.webm")
+#system("ffmpeg -f concat -i presentation-timestamps -c:v libvpx-vp9 -b:v 2M -pix_fmt yuva420p -metadata:s:v:0 alpha_mode=\"1\" -vsync vfr -auto-alt-ref 0 -y -filter_complex 'scale=w=1280:h=720:force_original_aspect_ratio=1,pad=1280:720:-1:-1:white' presentation.webm")
 
 # Recreates the whiteboard annotations
-system("ffmpeg -f concat -i whiteboard-timestamps -c:v libvpx-vp9 -b:v 2M -pix_fmt yuva420p -metadata:s:v:0 alpha_mode=\"1\" -vsync vfr -auto-alt-ref 0 -y -filter_complex 'scale=w=1280:h=720:force_original_aspect_ratio=1,pad=1280:720:-1:-1:white' whiteboard.webm")
+#system("ffmpeg -f concat -i whiteboard-timestamps -c:v libvpx-vp9 -b:v 2M -pix_fmt yuva420p -metadata:s:v:0 alpha_mode=\"1\" -vsync vfr -auto-alt-ref 0 -y -filter_complex 'scale=w=1280:h=720:force_original_aspect_ratio=1,pad=1280:720:-1:-1:white' whiteboard.webm")
 
 # Presentation + Annotations + Deskshare + Webcams (.webm)
-system("ffmpeg -i deskshare/deskshare.mp4 -c:v libvpx-vp9 -i presentation.webm -c:v libvpx-vp9 -i whiteboard.webm -i video/webcams.mp4 -filter_complex '[3]scale=w=iw/4:h=ih/4[cam];[0][1]overlay[tmp];[tmp][2]overlay[out];[out][cam]overlay=x=(main_w-overlay_w)' -b:v 2M -shortest -y presentation-deskshare-webcam.webm")
+#system("ffmpeg -i deskshare/deskshare.mp4 -c:v libvpx-vp9 -i presentation.webm -c:v libvpx-vp9 -i whiteboard.webm -i video/webcams.mp4 -filter_complex '[3]scale=w=iw/4:h=ih/4[cam];[0][1]overlay[tmp];[tmp][2]overlay[out];[out][cam]overlay=x=(main_w-overlay_w)' -b:v 2M -shortest -y presentation-deskshare-webcam.webm")
 
 # Presentation + Annotations + Deskshare + Webcams (.mp4) - faster, patent encumbered
 #system("ffmpeg -i deskshare/deskshare.mp4 -c:v libvpx-vp9 -i presentation.webm -c:v libvpx-vp9 -i whiteboard.webm -i video/webcams.mp4 -filter_complex '[3]scale=w=iw/4:h=ih/4[cam];[0][1]overlay[tmp];[tmp][2]overlay[out];[out][cam]overlay=x=(main_w-overlay_w)' -shortest -y presentation-deskshare-webcam.mp4")

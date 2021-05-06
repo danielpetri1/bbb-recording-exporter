68ee3de4df1faf9c93538345be6e9e3b0e1b987d
Daniel Petri Rocha <daniel.petri@tum.de>
2021-05-06 17:13:39 +0200

    Updated README and improved code readability

--- download_client.rb
+++ download_client.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Reference recording: "https://balancer.bbb.rbg.tum.de/playback/presentation/2.3/f5c1fdc86039b1cd48cb686d38ec0eb6be27dfc7-1619030802001?meetingId=f5c1fdc86039b1cd48cb686d38ec0eb6be27dfc7-1619030802001"
+# Reference recording "https://balancer.bbb.rbg.tum.de/playback/presentation/2.3/f5c1fdc86039b1cd48cb686d38ec0eb6be27dfc7-1619030802001?meetingId=f5c1fdc86039b1cd48cb686d38ec0eb6be27dfc7-1619030802001"
 require 'nokogiri'
 require 'open-uri'
 require 'cgi'
@@ -18,12 +18,16 @@ def download(file)
 
   puts "Downloading #{path}"
 
-  open(file, 'wb') do |file|
-    file << open(path).read
+  File.open(file, 'wb') do |get|
+    get << URI.parse(path.to_s).open(&:read)
   end
 end
 
-# Download desired data, e.g. 'metadata.xml', 'panzooms.xml', 'cursor.xml', 'presentation_text.json', 'captions.json', 'slides_new.xml', 'video/webcams.mp4', 'deskshare/deskshare.mp4'
+# Downloads the recording's assets
+# Whiteboard: 'shapes.svg', 'cursor.xml', 'panzooms.xml', 'presentation_text.json', 'captions.json', 'metadata.xml'
+# Video: 'video/webcams.mp4', 'deskshare/deskshare.mp4'
+# Chat: 'slides_new.xml'
+
 ['shapes.svg', 'video/webcams.mp4', 'deskshare/deskshare.mp4'].each do |get|
   download(get)
 end

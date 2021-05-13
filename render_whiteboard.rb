ec47059dfccfd93c4664fb3893dba03fb7d284d1
Daniel Petri Rocha <daniel.petri@tum.de>
2021-05-13 21:38:59 +0200

    Rubocop prettify

--- render_whiteboard.rb
+++ render_whiteboard.rb
@@ -18,7 +18,7 @@ intervals = (ins + outs + timestamps + undos).to_a.map(&:to_s).map(&:to_f).uniq.
 # Update image paths since files are saved in ./frames
 images.each do |image|
   path = image.attr('xlink:href')
-  image.set_attribute('xlink:href', '../' + path)
+  image.set_attribute('xlink:href', "../#{path}")
   image.set_attribute('style', 'visibility:visible')
 end
 
@@ -47,15 +47,17 @@ frames.each do |frame|
   # Get slide information
   slide_id = slide.attr('id').to_s
   slide_class = slide.attr('class').to_s
-  
+
   width = slide.attr('width').to_s
   height = slide.attr('height').to_s
   x = slide.attr('x').to_s
   y = slide.attr('y').to_s
 
-  viewBox = '0 0 1600 900'
+  view_box = '0 0 1600 900'
 
-  draw = @doc.xpath('//xmlns:g[@class="canvas" and @image="' + slide_id.to_s + '"]/xmlns:g[@timestamp < "' + interval_end.to_s + '" and (@undo = "-1" or @undo >= "' + interval_end.to_s + '")]', 'xmlns' => 'http://www.w3.org/2000/svg')
+  draw = @doc.xpath(
+    "//xmlns:g[@class=\"canvas\" and @image=\"#{slide_id}\"]/xmlns:g[@timestamp < \"#{interval_end}\" and (@undo = \"-1\" or @undo >= \"#{interval_end}\")]", 'xmlns' => 'http://www.w3.org/2000/svg'
+  )
 
   # Removes redundant frames that compose the shape's animation
   # For backwards-compability with BBB 2.2, live whiteboard not supported in BBB 2.3
@@ -63,41 +65,35 @@ frames.each do |frame|
   shapes = draw.xpath('./@shape')
 
   shapes.each do |shape|
-    
     # Remove every frame that led up to the final state of the annotation
     delete = shape.xpath('parent::*')
     keep = delete.last
-    draw = draw - delete
+    draw -= delete
 
     # Add final state of the shape
     draw.push(keep)
-
   end
 
   # -----------------------------------
 
   # Builds SVG frame
-  builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
-    
-    xml.doc.create_internal_subset('svg','-//W3C//DTD SVG 1.1//EN','http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd')
+  builder = Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
+    xml.doc.create_internal_subset('svg', '-//W3C//DTD SVG 1.1//EN', 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd')
 
-    xml.svg(width: width, height: height, x: x, y: y, version: '1.1', viewBox: viewBox, 'xmlns' => 'http://www.w3.org/2000/svg', 'xmlns:xlink' => 'http://www.w3.org/1999/xlink') do
-      
+    xml.svg(width: width, height: height, x: x, y: y, version: '1.1', viewBox: view_box, 'xmlns' => 'http://www.w3.org/2000/svg', 'xmlns:xlink' => 'http://www.w3.org/1999/xlink') do
       # Display background image
       xml.image(id: slide_id, class: slide_class, 'xlink:href': slide.attr('href'), width: width, height: height, x: x, y: y, style: slide.attr('style'))
 
       # Add annotations
       draw.each do |shape|
-
         # Make shape visible
         style = shape.attr('style')
         style.sub! 'hidden', 'visible'
-        #shape.set_attribute('style', style)
-        
+        # shape.set_attribute('style', style)
+
         xml.g(id: shape.attr('id'), style: style) do
           xml << shape.xpath('./*').to_s
         end
-
       end
     end
   end

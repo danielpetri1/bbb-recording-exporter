9d2fd4f6f36ae37e16257c5124977b2f06deb455
Daniel Petri Rocha <daniel.petri@tum.de>
2021-06-22 23:07:02 +0200

    Interval tree data structure

--- download_client.rb
+++ download_client.rb
@@ -12,7 +12,7 @@ def download(file)
   # path = "https://vmott40.in.tum.de/presentation/fccbbfd5ae98f6eb1e6bf57fc8970672da7244b6-1623321256773/#{file}"
 
   # Lasttest
-  path = "https://vmott40.in.tum.de/presentation/b87bbe0888dff19aa181be51d86a3f52543fc5a7-1620820241322/#{file}"
+  #path = "https://vmott40.in.tum.de/presentation/b87bbe0888dff19aa181be51d86a3f52543fc5a7-1620820241322/#{file}"
 
   # Pink Cube
   path = "https://vmott40.in.tum.de/presentation/b87bbe0888dff19aa181be51d86a3f52543fc5a7-1620639249054/#{file}"
--- render_whiteboard.rb
+++ render_whiteboard.rb
@@ -4,6 +4,7 @@
 require 'nokogiri'
 require 'base64'
 require 'zlib'
+require 'itree'
 
 def base64_encode(path)
   data = File.open(path).read
@@ -86,8 +87,12 @@ pan_reader = Nokogiri::XML::Reader(File.open('panzooms.xml'))
 
 view_boxes = []
 reader_timestamps = []
+
 slides = []
+slides_interval_tree = Intervals::Tree.new
+
 shapes = []
+shapes_interval_tree = Intervals::Tree.new
 
 # Parse recording intervals
 pan_reader.each do |node|
@@ -107,11 +112,13 @@ end
 # Get array containing [panzoom timestamp, view_box parameter]
 panzooms = reader_timestamps.zip(view_boxes)
 
+slide_out = 0
+slide_in = 0
+
 shape_reader.each do |node|
   if node.node_type == Nokogiri::XML::Reader::TYPE_ELEMENT then
     
     if node.name == 'image' && node.attribute('class') == 'slide' then
-      
       slide_in = node.attribute('in').to_f
       slide_out = node.attribute('out').to_f
 
@@ -120,7 +127,10 @@ shape_reader.each do |node|
 
       # Image paths need to follow the URI Data Scheme (for slides and polls)
       path = node.attribute('href')
-      slides << [node.attribute('id').to_s, base64_encode(path), slide_in, slide_out, node.attribute('width').to_f, node.attribute('height')]
+
+      # slides << [node.attribute('id').to_s, base64_encode(path), slide_in, slide_out, node.attribute('width').to_f, node.attribute('height')]
+
+      slides_interval_tree.insert(slide_in, slide_out, [base64_encode(path), node.attribute('width').to_f, node.attribute('height')])
 
     end
 
@@ -133,11 +143,43 @@ shape_reader.each do |node|
       reader_timestamps << shape_timestamp
       reader_timestamps << shape_undo
 
-      shapes << [shape_id, shape_timestamp, shape_undo, node.attribute('style'), node.inner_xml]
+      if shape_undo < 0 then
+        shape_undo = slide_out
+      end
+
+      shape_enter = [[shape_timestamp, slide_in].max, slide_out].min
+      shape_leave = [[shape_undo, slide_in].max, slide_out].min
+
+      #if shape_id == 'image5' then
+        #puts "timestamp " + shape_timestamp.to_s
+        #puts "in " + slide_in.to_s
+        #puts "-----"
+        #puts "undo " + shape_undo.to_s
+        #puts "out " + slide_out.to_s
+        #puts node.attribute('id')
+        #puts "===="
+
+        #puts shape_enter < shape_leave
+        #puts shape_enter
+        #puts shape_leave
+        #puts "----"
+
+        #puts node.inner_xml
+        #shapes_interval_tree.insert(shape_enter, shape_leave, "A")
+      #end
+      
+      # shapes << [shape_id, shape_timestamp, shape_undo, node.attribute('style'), node.inner_xml]
+
+      if shape_enter < shape_leave then
+        shapes_interval_tree.insert(shape_enter, shape_leave, [node.attribute('style'), node.inner_xml])
+      end
+
     end
   end
 end
 
+puts shapes_interval_tree.stab(48.0, 49.7)[0].data
+i = 1/0
 intervals = reader_timestamps.uniq.sort
 
 # Intervals with a value of -1 do not correspond to a timestamp
@@ -154,6 +196,7 @@ end
 # Render the visible frame for each interval
 File.open('timestamps/whiteboard_timestamps', 'w') do |file|
   frames.each do |frame|
+
     interval_start = frame[0]
     interval_end = frame[1]
 
@@ -171,15 +214,24 @@ File.open('timestamps/whiteboard_timestamps', 'w') do |file|
     view_box = panzooms.first[1]
 
     # Get slide information
-    slide = slides.find_index { |_, _, slide_in, slide_out, _, _| slide_in <= interval_start && interval_end <= slide_out}
-
-    slide_id = slides[slide][0]
-    slide_href = slides[slide][1]
-    slide_out = slides[slide][3]
-    width = slides[slide][4]
-    height = slides[slide][5]
+    slide = slides_interval_tree.stab(interval_start, interval_end)
     
-    draw = shapes.select { |id, t, undo, _, _| id == slide_id && t < interval_end && (undo == -1 || undo >= interval_end)}
+    slide_in, slide_out = slide.first.scores
+    slide_href, slide_width, slide_height = slide.first.data
+  
+    draw = shapes_interval_tree.stab(interval_start, interval_end)
+
+    # puts interval_start
+    # puts interval_end
+
+    # puts draw.length
+
+    # draw.each do |annotation|
+      # puts annotation.data
+      # puts "===="
+    # end
+
+    #draw = shapes.select { |id, t, undo, _, _| id == slide_id && t < interval_end && (undo == -1 || undo >= interval_end)}
 
     # draw = @doc.xpath("svg/g[@class=\"canvas\" and @image=\"#{slide_id}\"]/g[@timestamp < \"#{interval_end}\" and (@undo = \"-1\" or @undo >= \"#{interval_end}\")]")
 
@@ -189,15 +241,22 @@ File.open('timestamps/whiteboard_timestamps', 'w') do |file|
       xml.svg(width: "1600", height: "1080", viewBox: view_box, 'xmlns' => 'http://www.w3.org/2000/svg') do
       
         # Display background image
-        xml.image('href': slide_href, width: width, height: height, preserveAspectRatio: "xMidYMid slice")
+        xml.image('href': slide_href, width: slide_width, height: slide_height, preserveAspectRatio: "xMidYMid slice")
       
         # Adds annotations
-        draw.each do |annotation|
-          xml.g('style': annotation[3]) do
-            xml << annotation[4]
-          end 
-        end
+        #puts "Interval start: " + interval_start.to_s
+        #puts "Interval end: " + interval_end.to_s
 
+        draw.each do |annotation|
+          xml.g('style': annotation.data.first) do
+              #puts annotation.scores
+              #puts "-----"
+              
+
+              xml << annotation.data[1]
+          end
+        end
+        #puts "==="
       end
     end
 
@@ -220,15 +279,16 @@ File.open('timestamps/whiteboard_timestamps', 'w') do |file|
     frame_number += 1
 
     # Remove canvas once we're done with it
-    if slide_out == interval_end && !draw.empty? then
+    #if slide_out == interval_end && !draw.empty? then
 
-      next_canvas = shapes.find_index {|id, _, _, _, _| slide_id != id}
-      next_canvas = 0 if next_canvas.nil?
+      # next_canvas = shapes.find_index {|id, _, _, _, _| slide_id != id}
+      # next_canvas = 0 if next_canvas.nil?
 
-      shapes = shapes.slice!(next_canvas, shapes.length - 1)
+      # shapes = shapes.slice!(next_canvas, shapes.length - 1)
 
-    end
-
+    #end
+    #slides_interval_tree.remove(interval_start, interval_end)
+    #shapes_interval_tree.remove(interval_start, interval_end)
   end
 
   # The last image needs to be specified twice, without specifying the duration (FFmpeg quirk)

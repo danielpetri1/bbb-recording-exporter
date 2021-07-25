e00c13f4701f5e0e7293ae3f460c507e010d91e9
Daniel Petri Rocha <daniel.petri@tum.de>
2021-07-25 16:09:37 +0200

    Export presentation slides as PDF

--- README.md
+++ README.md
@@ -1,23 +1,7 @@
 
 # BigBlueButton Exporter
 
-Client-side version of the script that enables users to download BigBlueButton 2.3 recordings as a single video file.
-
-  
-
-## What's supported?
-
-✅ Annotations <br  />
-✅ Chat <br  />
-✅ Cursor <br  />
-✅ Polls <br  />
-✅ Screen shares <br  />
-✅ Slides<br  />
-✅ Text <br  />
-✅ Webcams <br  />
-✅ Zooms <br  />
-
-![BigBlueButton recording exporter](/demo/export_example.png)
+Client-side version of the script to export the recorded presentation as a single PDF file.
 
 ## Client Side Usage
 
@@ -25,31 +9,24 @@ In the `download_client.rb` file, change the `path` variable to reflect your BBB
 
     path = "https://hostname/presentation/meetingID/#{file}"
 
-Make sure to adapt the array beginning with `shapes.svg` to reflect your recording's data, changing file extensions and removing the deskshare if necessary.
-
 Run with
 
     ruby download_client.rb
 
-Render the presentation, whiteboard, mouse pointer and chat with
+Export the PDF with
 
-    ruby export_presentation.rb
+    ruby export_slides.rb
 
+The resulting file `annotated_slides.pdf` can then be found in the script's directory.
 
 ### Requirements
 
-FFmpeg compiled with `--enable-librsvg` and `--enable-libx264` <br  />
-
-Ruby with the Nokogiri and Loofah gems installed <br  />
+librsvg <br  />
+combine_pdf gem <br  />
 
 ###  Rendering options 
-If your server runs BBB 2.2 or earlier, it is advised to set the flag `REMOVE_REDUNDANT_SHAPES` to **true** in `export_presentation.rb`. This will ensure the live whiteboard feature is still supported, require less storage space and increase rendering speeds.
-
+If your server runs BBB 2.2 or earlier, it is advised to set the flag `REMOVE_REDUNDANT_SHAPES` to **true** in `export_slides.rb`.
 Less data can be written on the disk by turning `SVGZ_COMPRESSION` on.
-
-To make rendering faster and less resource-intensive, download FFMpeg's source code and replace the file `ffmpeg/libavcodec/librsvgdec.c` with the one in this directory. After compiling and installing FFMpeg, enable `FFMPEG_REFERENCE_SUPPORT` in `export_presentation.rb` .
-
-The video output quality can be controlled with the `CONSTANT_RATE_FACTOR`.
 
 ###  Get in touch
 If this code helped you or  you encountered any problems, please do get in touch! Since this script is being actively developed for my bachelor's Thesis at the Technical University of Munich, feedback is more than welcomed.
---
+++ export_slides.rb
@@ -0,0 +1,270 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: false
+
+require "base64"
+require "builder"
+require "combine_pdf"
+require "fileutils"
+require "loofah"
+require "nokogiri"
+require "zlib"
+
+require_relative "lib/interval_tree"
+include IntervalTree
+
+@published_files = File.expand_path(".")
+
+# Creates scratch directories
+Dir.mkdir("#{@published_files}/frames") unless File.exist?("#{@published_files}/frames")
+
+# Setting the SVGZ option to true will write less data on the disk.
+SVGZ_COMPRESSION = false
+
+# Set this to true if the svg to pdf converter supports external references
+REFERENCE_SUPPORT = false
+BASE_URI = REFERENCE_SUPPORT ? "-base_uri #{@published_files}" : ""
+
+FILE_EXTENSION = SVGZ_COMPRESSION ? "svgz" : "svg"
+
+# Leave it as false for BBB >= 2.3 as it stopped supporting live whiteboard
+REMOVE_REDUNDANT_SHAPES = false
+
+WhiteboardElement = Struct.new(:begin, :end, :value, :id)
+WhiteboardSlide = Struct.new(:href, :begin, :end, :width, :height)
+
+def base64_encode(path)
+  data = File.open(path).read
+  "data:image/#{File.extname(path).delete('.')};base64,#{Base64.strict_encode64(data)}"
+end
+
+def convert_whiteboard_shapes(whiteboard)
+  # Find shape elements
+  whiteboard.xpath("svg/g/g").each do |annotation|
+    # Make all annotations visible
+    style = annotation.attr("style")
+    style.sub! "visibility:hidden", ""
+    annotation.set_attribute("style", style)
+
+    shape = annotation.attribute("shape").to_s
+    # Convert polls to data schema
+    if shape.include? "poll"
+      poll = annotation.element_children.first
+
+      path = "#{@published_files}/#{poll.attribute('href')}"
+      poll.remove_attribute("href")
+
+      # Namespace xmlns:xlink is required by FFmpeg
+      poll.add_namespace_definition("xlink", "http://www.w3.org/1999/xlink")
+
+      data = REFERENCE_SUPPORT ? "file:///#{path}" : base64_encode(path)
+
+      poll.set_attribute("xlink:href", data)
+    end
+
+    # Convert XHTML to SVG so that text can be shown
+    next unless shape.include? "text"
+
+    # Turn style attributes into a hash
+    style_values = Hash[*CSV.parse(style, col_sep: ":", row_sep: ";").flatten]
+
+    # The text_color variable may not be required depending on your FFmpeg version
+    text_color = style_values["color"]
+    font_size = style_values["font-size"].to_f
+
+    annotation.set_attribute("style", "#{style};fill:currentcolor")
+
+    foreign_object = annotation.xpath("switch/foreignObject")
+
+    # Obtain X and Y coordinates of the text
+    x = foreign_object.attr("x").to_s
+    y = foreign_object.attr("y").to_s
+    text_box_width = foreign_object.attr("width").to_s.to_f
+
+    text = foreign_object.children.children
+
+    builder = Builder::XmlMarkup.new
+    builder.text(x: x, y: y, fill: text_color, "xml:space" => "preserve") do
+      text.each do |line|
+        line = line.to_s
+
+        if line == "<br/>"
+          builder.tspan(x: x, dy: "0.9em") { builder << "<br/>" }
+        else
+          # Assumes a width to height aspect ratio of 0.52 for Arial
+          line_breaks = line.chars.each_slice((text_box_width / (font_size * 0.52)).to_i).map(&:join)
+
+          line_breaks.each do |row|
+            safe_message = Loofah.fragment(row).scrub!(:escape).unicode_normalize
+            builder.tspan(x: x, dy: "0.9em") { builder << safe_message }
+          end
+        end
+      end
+    end
+
+    annotation.add_child(builder.target!)
+
+    # Remove the <switch> tag
+    annotation.xpath("switch").remove
+  end
+
+  # Save new shapes.svg copy
+  File.open("#{@published_files}/shapes_modified.svg", "w", 0o600) do |file|
+    file.write(whiteboard)
+  end
+end
+
+def parse_whiteboard_shapes(shape_reader)
+  slide_in = 0
+  slide_out = 0
+
+  shapes = []
+  slides = []
+
+  shape_reader.each do |node|
+    next unless node.node_type == Nokogiri::XML::Reader::TYPE_ELEMENT
+
+    node_name = node.name
+    node_class = node.attribute("class")
+
+    if node_name == "image" && node_class == "slide"
+      slide_in = node.attribute("in").to_f
+      slide_out = node.attribute("out").to_f
+
+      # Image paths need to follow the URI Data Scheme (for slides and polls)
+      path = "#{@published_files}/#{node.attribute('href')}"
+
+      next if path.include?('deskshare')
+
+      data = REFERENCE_SUPPORT ? "file:///#{path}" : base64_encode(path)
+
+      slides << WhiteboardSlide.new(data, slide_in, slide_out, node.attribute("width").to_f, node.attribute("height"))
+    end
+
+    next unless node_name == "g" && node_class == "shape"
+
+    shape_timestamp = node.attribute("timestamp").to_f
+    shape_undo = node.attribute("undo").to_f
+
+    shape_undo = slide_out if shape_undo.negative?
+
+    shape_enter = [[shape_timestamp, slide_in].max, slide_out].min
+    shape_leave = [[shape_undo, slide_in].max, slide_out].min
+
+    xml = "<g style=\"#{node.attribute('style')}\">#{node.inner_xml}</g>"
+    id = node.attribute("shape").split("-").last
+
+    shapes << WhiteboardElement.new(shape_enter, shape_leave, xml, id)
+  end
+
+  [shapes, slides]
+end
+
+def remove_adjacent(array)
+  index = 0
+
+  until array[index + 1].nil?
+    array[index] = nil if array[index].id == array[index + 1].id
+    index += 1
+  end
+
+  array.compact
+end
+
+def render_whiteboard(slides, shapes)
+  shapes_interval_tree = IntervalTree::Tree.new(shapes)
+  frame_number = 0
+
+  merged = CombinePDF.new
+
+  slides.each do |slide|
+    draw = shapes_interval_tree.search(slide.end - 0.05, unique: false, sort: false)
+    draw = [] if draw.nil?
+
+    if draw.nil?
+      draw = []
+    elsif REMOVE_REDUNDANT_SHAPES && !draw.empty?
+      draw = remove_adjacent(draw)
+    end
+
+    svg_export(draw, slide.href, slide.width, slide.height, frame_number)
+
+    pdf = system("rsvg-convert -f pdf -o frames/frame#{frame_number}.pdf frames/frame#{frame_number}.#{FILE_EXTENSION}")
+
+    unless pdf
+      warn("An error occurred generating the PDF for slide #{frame_number}")
+      exit(false)
+    end
+
+    merged << CombinePDF.load("frames/frame#{frame_number}.pdf")
+
+    frame_number += 1
+  end
+
+  merged.save "annotated_slides.pdf"
+end
+
+def svg_export(draw, slide_href, width, height, frame_number)
+  # Builds SVG frame
+  builder = Builder::XmlMarkup.new
+
+  builder.svg(width: width, height: height, viewBox: "0 0 #{width} #{height}",
+              "xmlns:xlink" => "http://www.w3.org/1999/xlink", 'xmlns' => 'http://www.w3.org/2000/svg') do
+    # Display background image
+    builder.image('xlink:href': slide_href, width: width, height: height)
+
+    # Adds annotations
+    draw.each do |shape|
+      builder << shape.value
+    end
+  end
+
+  File.open("#{@published_files}/frames/frame#{frame_number}.#{FILE_EXTENSION}", "w", 0o600) do |svg|
+    if SVGZ_COMPRESSION
+      svgz = Zlib::GzipWriter.new(svg)
+      svgz.write(builder.target!)
+      svgz.close
+    else
+      svg.write(builder.target!)
+    end
+  end
+end
+
+def unique_slides(slides)
+  # Only keep the last state of the slides, maintaining original order
+  for i in 0..slides.size - 1 do
+    for j in (i + 1)..slides.size - 1 do
+      next if slides[i].nil? || slides[j].nil?
+      if slides[i].href == slides[j].href then
+        slides[i] = slides[j]
+        slides[j] = nil
+      end
+    end
+  end
+
+  return slides.compact
+end
+
+def export_pdf
+  # Benchmark
+  start = Time.now
+
+  puts "Started exporting PDF"
+
+  convert_whiteboard_shapes(Nokogiri::XML(File.open("#{@published_files}/shapes.svg")).remove_namespaces!)
+
+  shapes, slides = parse_whiteboard_shapes(Nokogiri::XML::Reader(File.open("#{@published_files}/shapes_modified.svg")))
+  slides = unique_slides(slides)
+
+  render_whiteboard(slides, shapes)
+
+  puts "Finished exporting PDF. Total: #{Time.now - start}"
+
+  start = Time.now
+end
+
+export_pdf
+
+# Delete the contents of the scratch directories
+FileUtils.rm_rf(["#{@published_files}/frames", "#{@published_files}/shapes_modified.svg"])
+
+exit(0)

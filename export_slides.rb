2ad0eb788bb9ed701d725235957d71882574a435
Daniel Petri Rocha <daniel.petri@tum.de>
2021-08-02 15:20:01 +0200

    Removed redundant nil check

--- export_slides.rb
+++ export_slides.rb
@@ -196,7 +196,6 @@ def render_whiteboard(slides, shapes)
 
   slides.each do |slide|
     draw = shapes_interval_tree.search(slide.end - 0.05, unique: false, sort: false)
-    draw = [] if draw.nil?
 
     if draw.nil?
       draw = []
